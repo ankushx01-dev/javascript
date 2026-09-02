b14ad06c7db129d92c992b305f21e66a83209830
ankushx01-dev <rajputx000@gmail.com>
2026-09-02 17:57:08 +0530

    DOM project

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
-function maskEmail(email){
-  let mail=email.indexOf('@')
-  let newEmail =email.slice(mail);
-  let firstWord= email[0]
-  let lastWord=email[mail-1]
-  
-  let star ="*"
-  let n =mail-2
-  let rep = star.repeat(n)
-    return firstWord +rep+lastWord + newEmail
-}
-
-let email ="apple.pie@example.com";
-console.log(maskEmail(email));
-email="freecodecamp@example.com"
-console.log(maskEmail(email));
-email="info@test.dev"
-console.log(maskEmail(email));
-email="user@domain.org"
-console.log(maskEmail(email));
-
-
+const clock=document.querySelector('#clock');
+setInterval(function(){
+  let date=new Date()
+  clock.innerHTML = date.toLocaleTimeString()
+},1000)
